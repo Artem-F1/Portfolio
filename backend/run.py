b2000545b0ef79fb6ad6d2bdeaaa2ef1976deb02
Artem-F1 <tainaartem@gmail.com>
2026-08-19 18:06:32 +1000

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -2,10 +2,10 @@ import uvicorn
 import os
 
 if __name__ == "__main__":
-    reload = os.getenv("ENV", "development") == "development"
+    port = int(os.getenv("PORT", 8000))
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
-        port=8000,
+        port=port,
         reload=False
     )
